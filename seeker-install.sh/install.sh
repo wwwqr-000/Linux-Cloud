6c06736325568c07c61a2365988898f8148f3cf5
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2021-10-21 12:23:47 -0500

    Add files via upload

---
+++ seeker-install.sh/install.sh
@@ -0,0 +1,10 @@
+#file by Whiteavocado
+sudo git clone https://github.com/wwwqr-000/Linux-Cloud/tree/main/seeker.git
+sudo cd seeker/
+sudo apt update
+sudo apt install python3 python3-pip php
+sudo pip3 install requests
+sudo chmod +x install.sh && ./install.sh
+sudo apt install apache2 -y
+sudo service apache2 restart
+sudo ./seeker.py -t manual
